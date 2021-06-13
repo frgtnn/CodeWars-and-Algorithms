556c1fb29b62b8e62c1ae5c26f66644a83481105
frgtnn <rexxdragon11@gmail.com>
2021-06-13 16:17:35 +0200

    Odd or even

---
+++ Odd or even/script.js
@@ -0,0 +1,13 @@
+function oddOrEven(array) {
+    var result = 0;
+    for (var i = 0; i < array.length; i++){
+        result+=array[i];
+    } if (result%2 == 0) {
+        return "even";
+    } else {
+     return "odd"; 
+    }
+}
+
+result = oddOrEven([1,2,4])
+console.log(result)
