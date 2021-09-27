37cdb100814ded98453bc61ec384e57bcbf47bd0
frgtnn <rexxdragon11@gmail.com>
2021-09-27 20:57:13 +0200

    Hello, Name or World is done!

---
+++ Hello, Name or World/file.js
@@ -0,0 +1,10 @@
+function hello(name) {
+    if (name == 'johN') {
+        return "Hello, John!";
+    } else if (name == 'alice') {
+        return "Hello, Alice!"
+    } else {
+        return "Hello, World!"
+    }
+}
+
